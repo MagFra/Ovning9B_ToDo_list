37ed27496f9e303fa32fda16065cd126204d2bde
MagFra <47413425+magfra@users.noreply.github.com>
2023-10-24 10:14:58 +0200

    Så långt hann jag!

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,53 @@
 const form = document.querySelector('#inputForm');
 const formBtn = document.querySelector('#formBtn');
 const output = document.querySelector('#output');
+// ================================================
+const list = []; // To store the ToDo:s.
+// ================================================
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    toList();
+
+    e.target.reset();
+
+    console.log(list);
+})
+
+
+// ================================================
+function toList() {
+    let text = document.querySelector('#text').value;
+    let klart = document.querySelector('#klart').value;
+    list.push(new ToDo(text, klart));
+
+    // Anropa "Skriv ut" läggtill till lista.
+}
+// ================================================
+
+
+// ================================================
+function formatera(index) {
+    let struken = '';
+    if (list[+index].done) {
+        struken = ' struken';
+    }
+    return `<div id="${+index}" class="todo${struken}">${list[+index].output}</div>`;
+}
+// ================================================
+
+
+// ================================================
+class ToDo {
+    done = false;
+    constructor(text, datum) {
+        this.text = text;
+        this.datum = datum;
+    }
+
+    output() {
+        return `<p>${this.text}</p><p>${this.datum}</p>`;
+    }
+}
